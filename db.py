ad77e2fd2bd8d6d94edcaddadaaaeabf6ceb945b
paulikarels <pauli.karels@gmail.com>
2022-09-25 21:19:26 +0300

    add base structure

---
+++ db.py
@@ -0,0 +1,10 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
+db = SQLAlchemy(app)
