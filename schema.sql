5d9297f9bd70e8eaf05c9f8028cc308eac5daae9
paulikarels <pauli.karels@gmail.com>
2022-09-25 21:40:17 +0300

    remove unnecessary comments

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
-/*CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
-*/
+
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
@@ -8,11 +7,7 @@ CREATE TABLE users (
     password TEXT 
 );
 
---credits?
--- INSERT INTO courses (userID, title, credits) VALUES (1, 'Kokkailun perusteet', 2);
--- INSERT INTO courses (userID, title, credits) VALUES (1, 'Advanced laiskottelu', 15);
--- CREATE SEQUENCE id_seq;
--- ALTER TABLE courses ALTER COLUMN id SET DEFAULT nextval('id_seq');
+
 CREATE TABLE  courses (
     id SERIAL PRIMARY KEY,
     userID INTEGER REFERENCES users ON DELETE CASCADE,
@@ -34,12 +29,9 @@ CREATE TABLE courseInstructors (
     userID INTEGER REFERENCES users ON DELETE CASCADE
 );
 
---exercises/assigments
--- INSERT INTO exercises (courseID, description) VALUES (1, 'Keittaminen');
--- INSERT INTO exercises (courseID, description) VALUES (1, 'Paistaminen');
--- INSERT INTO exercises (courseID, description) VALUES (2, 'Makuuasento');
 
---consider deleteing DONE
+
+--consider deleting DONE
 CREATE TABLE exercises (
     id SERIAL PRIMARY KEY,
     courseID INTEGER REFERENCES courses ON DELETE CASCADE,
@@ -48,11 +40,7 @@ CREATE TABLE exercises (
     --answer TEXT,
     --correctAnswer TEXT
 );
-/*
-INSERT INTO questions (exercisesID, question, answer) VALUES (1, 'Missa yleensa keitetaan kananmunat?', 'vedessa');
-INSERT INTO questions (exercisesID, question, answer) VALUES (1, 'Mika on veden kiehumispiste?', 100);
-INSERT INTO questions (exercisesID, question, answer) VALUES (1, 'Tama on vain testi?', 'test');
-*/
+
 CREATE TABLE questions (
     id SERIAL PRIMARY KEY,
     exercisesID INTEGER REFERENCES exercises ON DELETE CASCADE,
@@ -60,29 +48,14 @@ CREATE TABLE questions (
     answer TEXT
 );
 
-/*
-INSERT INTO quizzes (questionID, quiz) VALUES (1, 'vedessa');
-INSERT INTO quizzes (questionID, quiz) VALUES (1, 'Coca-Colassa');
-INSERT INTO quizzes (questionID, quiz) VALUES (1, 'Saippuassa');
 
-INSERT INTO quizzes (questionID, quiz) VALUES (2, 80);
-INSERT INTO quizzes (questionID, quiz) VALUES (2, 100);
-
-INSERT INTO quizzes (questionID, quiz) VALUES (3, 'test');
-INSERT INTO quizzes (questionID, quiz) VALUES (3, 'no test');
- 
-*/
 CREATE TABLE quizzes (
     id SERIAL PRIMARY KEY,
     questionID INTEGER REFERENCES questions ON DELETE CASCADE,
     quiz TEXT
 );
 
-/*
-INSERT INTO userQuizAnswers (userID, questionID, quizID, correct) VALUES (1, 1, 1, true); 
-INSERT INTO userQuizAnswers (userID, questionID, quizID, correct) VALUES (1, 2, 4, false); 
 
-*/
 
 CREATE TABLE userQuizAnswers (
     userID INTEGER REFERENCES users ON DELETE CASCADE,
@@ -90,33 +63,3 @@ CREATE TABLE userQuizAnswers (
     quizID INTEGER REFERENCES quizzes ON DELETE CASCADE,
     correct BOOLEAN
 );
-
-/*
-SELECT * FROM courseQuestions;
-INSERT INTO  courseQuestions (courseID, questionID) VALUES (1, 1);
-INSERT INTO  courseQuestions (courseID, questionID) VALUES (1, 2);
- */
- /*
-CREATE TABLE courseQuestions (
-    courseID INTEGER REFERENCES courses ON DELETE CASCADE,
-    questionID INTEGER REFERENCES questions ON DELETE CASCADE
-);
-
-/*
-SELECT * FROM exerciseQuestions;
-INSERT INTO  exerciseQuestions (exerciseID, questionID) VALUES (1, 1);
-INSERT INTO  exerciseQuestions (exerciseID, questionID) VALUES (2, 2);
- */
- /*
-CREATE TABLE exerciseQuestions (
-    exerciseID INTEGER REFERENCES exercises ON DELETE CASCADE,
-    questionID INTEGER REFERENCES questions ON DELETE CASCADE
-);
-*/
-
-/*
-CREATE TABLE quiz (
-    id SERIAL PRIMARY KEY,
-    question TEXT
-);
-*/
